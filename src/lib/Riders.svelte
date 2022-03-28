311cd9a8970a141d4c55b72472f96fc488d039a7
pntbr <pntbr@pm.me>
2022-03-28 21:16:13 +0200

    vérifie et corrige les données incorrectes pour les randos

---
+++ src/lib/Riders.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let riders;
+  const ridersForceArray = Array.isArray(riders) || [];
+</script>
+
+{#if ridersForceArray.length}
+  <h5>Riders</h5>
+  <table>
+    <thead>
+      <tr>
+        <td>Name</td>
+        <td>Diet</td>
+        <td>Horse</td>
+      </tr>
+    </thead>
+    <tbody>
+      {#each ridersForceArray as rider}
+        <tr>
+          <td>{rider.firstname || '?'}</td>
+          <td>{rider.diet || '?'}</td>
+          <td>{rider.horse || '?'}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+{/if}
--- src/lib/Treck.svelte
+++ src/lib/Treck.svelte
@@ -1,12 +1,13 @@
 <script>
-	import Rider from '$lib/Rider.svelte';
-	export let treck;
+  import Riders from "$lib/Riders.svelte";
+  export let treck;
 </script>
 
 <article>
-	<header>
-		<h4>{treck.period}</h4>
-		<h6>{treck.title}</h6>
-	</header>
-	<Rider riders={treck.riders} />
+  <header>
+    <h4>{treck.period || '?'}</h4>
+    <h6>{treck.title || '?'}</h6>
+  </header>
+
+  <Riders riders={treck.riders} />
 </article>
