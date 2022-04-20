1a09236852ed481b6aadf8e990a08a7e2d9ed69c
pntbr <pntbr@pm.me>
2022-04-20 16:21:46 +0200

    affiche les nouveaux champs

--- src/lib/Riders.svelte
+++ src/lib/Riders.svelte
@@ -5,22 +5,17 @@
 
 {#if ridersForceArray.length}
   <h5>Riders</h5>
-  <table>
-    <thead>
-      <tr>
-        <td>Name</td>
-        <td>Diet</td>
-        <td>Horse</td>
-      </tr>
-    </thead>
-    <tbody>
-      {#each ridersForceArray as rider}
-        <tr>
-          <td>{rider.firstname || '?'}</td>
-          <td>{rider.diet || '?'}</td>
-          <td>{rider.horse || '?'}</td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
+  {#each ridersForceArray as rider}
+    <details>
+      <summary>{rider.firstname || '?'} - ({rider.country || '?'})</summary>
+      <ul>
+        <li>Diet: {rider.diet || '?'}</li>
+        <li>Age: {rider.age || '?'}</li>
+        <li>Weight: {rider.weight || '?'}</li>
+        <li>Height: {rider.height || '?'}</li>
+        <li>Relationship: {rider.relationship || '?'}</li>
+        <li>Horse: {rider.horse || '?'}</li>
+      </ul>
+    </details>
+  {/each}
 {/if}
