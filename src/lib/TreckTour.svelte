d7070a4e124a08443a6282bd0ceaf53d1785188d
pntbr <pntbr@pm.me>
2022-04-21 11:03:53 +0200

    affiche le tableau par nationalité

--- src/lib/TreckTour.svelte
+++ src/lib/TreckTour.svelte
@@ -1,6 +1,24 @@
 <script>
   import Riders from "$lib/Riders.svelte";
   export let treck;
+
+  const countrySummary = getCountrySummary(treck.riders);
+
+  function getCountrySummary(riders) {
+    if (!riders) return [];
+
+    const summary = {};
+
+    for (const rider of riders.map(rider => rider.country)) {
+      if (summary[rider]) {
+        summary[rider] += 1;
+      } else {
+        summary[rider] = 1;
+      }
+    }
+
+    return Object.entries(summary);
+  }
 </script>
 
 <article>
@@ -8,6 +26,10 @@
     <h4>{treck.period || '?'}</h4>
     <h6>{treck.title || '?'}</h6>
   </header>
-
-  <Riders riders={treck.riders} />
+  <h6>Countries</h6>
+  <ul>
+    {#each countrySummary as country}
+      <li>{country[0]}: {country[1]}</li>
+    {/each}
+  </ul>
 </article>
