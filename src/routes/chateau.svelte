e3b6d6df666dda280394a1c6e65ee66abaaac48f
pntbr <pntbr@pm.me>
2022-04-28 22:29:31 +0200

    installe un store pour le chateau

--- src/routes/chateau.svelte
+++ src/routes/chateau.svelte
@@ -1,42 +1,6 @@
-<script context="module">
-  export async function load() {
-    const jsonPath = {
-      path: "../lib/_data/2022/",
-      filenameSuffixe: "-week.json"
-    };
-
-    async function getTrecksData() {
-      async function importData(path) {
-        try {
-          const Post = await import(/* @vite-ignore */ path);
-          return Post.default;
-        } catch (e) {
-          return {
-            headling: "404",
-            content: "Page not found"
-          };
-        }
-      }
-
-      const weeksNumber = 35;
-      const trecks = [];
-
-      for (let i = 1; i <= weeksNumber; i++) {
-        trecks.push(
-          await importData(jsonPath.path + i + jsonPath.filenameSuffixe)
-        );
-      }
-
-      return trecks;
-    }
-
-    return { props: { trecks: await getTrecksData() } };
-  }
-</script>
-
 <script>
+  import { trecks } from "../stores";
   import Treck from "$lib/Treck.svelte";
-  export let trecks;
 </script>
 
 <style>
@@ -53,7 +17,7 @@
 
 <section>
   <div class="grid">
-    {#each trecks as treck}
+    {#each $trecks as treck}
       <Treck {treck} />
     {/each}
   </div>
---
+++ src/stores.js
@@ -0,0 +1,16 @@
+import {
+    readable
+} from 'svelte/store'
+
+const jsons =
+    import.meta.glob('./lib/_data/2022/*.json')
+
+const trecksContents = await Promise.all(Object.entries(jsons).map(async([path, resolver]) => {
+    const content = await resolver()
+
+    return content.default
+}))
+
+export let trecks = readable(null, (set) => {
+    set(trecksContents)
+})
