6f26776b5245ed4e36551f3bfd32257c8129de00
pntbr <pntbr@pm.me>
2022-04-28 22:32:27 +0200

    insert le store dans l'index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,42 +1,6 @@
-<script context="module">
-  export async function load() {
-    const jsonPath = {
-      path: "../lib/_data/2022/",
-      filenameSuffixe: "-week.json"
-    };
-
-    async function getTrecksData() {
-      async function importData(path) {
-        try {
-          const Post = await import(/* @vite-ignore */ path);
-          return Post.default;
-        } catch (e) {
-          return {
-            headling: "404",
-            content: "Page not found"
-          };
-        }
-      }
-
-      const weeksNumber = 35;
-      const trecks = [];
-
-      for (let i = 1; i <= weeksNumber; i++) {
-        trecks.push(
-          await importData(jsonPath.path + i + jsonPath.filenameSuffixe)
-        );
-      }
-
-      return trecks;
-    }
-
-    return { props: { trecks: await getTrecksData() } };
-  }
-</script>
-
 <script>
+  import { trecks } from "../stores";
   import TreckTour from "$lib/TreckTour.svelte";
-  export let trecks;
 </script>
 
 <style>
@@ -55,7 +19,7 @@
 
 <section>
   <div class="grid">
-    {#each trecks as treck}
+    {#each $trecks as treck}
       <TreckTour {treck} />
     {/each}
   </div>
