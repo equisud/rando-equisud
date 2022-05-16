35cede6940f84e51206e77d7ddebb47a28be482e
pntbr <pntbr@pm.me>
2022-05-16 14:07:43 +0200

    correction de pétouilles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,12 +7,6 @@
   const year = new Date().getFullYear();
 </script>
 
-<style>
-  .grid {
-    grid-template-columns: repeat(auto-fit, minmax(25%, 1fr));
-  }
-</style>
-
 <svelte:head>
   <title>Equisud riding {year} - Tour Operator</title>
 </svelte:head>
