9124ee7b38037e5b7cebad59f5dbdc5d64cd41c4
pntbr <pntbr@pm.me>
2023-03-03 23:57:52 +0100

    use store to display week

---
+++ src/routes/week/[slug]/+page.js
@@ -0,0 +1,25 @@
+import {
+    get
+} from 'svelte/store'
+import {
+    weeksLoad
+} from "$lib/stores.js";
+import {
+    periodToDate
+} from "$lib/db";
+
+export async function load({
+    params
+}) {
+    const weeks = get(weeksLoad)
+    const weekSelected = weeks.find(week => week.file === params.slug + '.json')
+
+    weekSelected.dates = periodToDate(weekSelected.period)
+    weekSelected.old = weekSelected.dates.arrival < Date.now()
+
+    return {
+        post: {
+            weekSelected
+        }
+    }
+}
--- src/routes/week/[slug]/+page.svelte
+++ src/routes/week/[slug]/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-  import WeekDetails from "$lib/WeekDetails.svelte"
+  import WeekDetails from "$lib/WeekDetails.svelte";
   export let data;
-  const week = data.post.week
+  const week = data.post.weekSelected;
 </script>
 
 <h1>Equisud - Treck board</h1>
