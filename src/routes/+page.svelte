1b2e79004199df22b761871e5277750586c90ce0
pntbr <pntbr@pm.me>
2023-04-11 12:56:14 +0200

    change l'accueil

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,48 @@
 <h1>Equisud riding 2023 - Tour Operator</h1>
+<img src="bandeau.jpg" alt="equisud" />
 <p>
   <a href="/weeks">To visualize the weeks</a>
 </p>
-<p>Contact us :</p>
+<hr />
+<p>
+  J'ai créé cette page pour simplifier notre planning. Vous y trouverez toutes
+  les informations nécessaires pour les futures randonnées et aurez toutes les
+  informations utiles pour connaître les dates et lieux des semaines qui auront
+  lieu cette année.
+</p>
+<ul>
+  <li>email: rando@equisud.com</li>
+  <li>mobile: +(33) 7 79 47 65</li>
+</ul>
+<hr />
+<p>
+  Ik heb deze pagina gemaakt om onze planning te vereenvoudigen. Je vindt er
+  alle nodige informatie voor toekomstige wandelingen en hebt alle nuttige
+  informatie om de data en plaats te kennen van de weken die dit jaar zullen
+  plaatsvinden.U kunt altijd contact met mij opnemen via
+</p>
+<ul>
+  <li>e-mail: rando@equisud.com</li>
+  <li>telefonisch: +(33) 7 79 47 65</li>
+</ul>
+<hr />
+<p>
+  Ich habe diese Seite erstellt, um unsere Planung zu vereinfachen. Sie finden
+  alle notwendigen Informationen für zukünftige Wanderungen und haben alle
+  nützlichen Informationen, um die Daten und den Ort der Wochen zu kennen, die
+  dieses Jahr stattfinden werden.Sie können mich jederzeit per
+</p>
+<ul>
+  <li>e-mail: rando@equisud.com</li>
+  <li>telefonisch: +(33) 7 79 47 65</li>
+</ul>
+<hr />
+<p>
+  I have created this page to simplify our planning. Here you will find all the
+  information you need for future walks and have all the information you need to
+  know about the dates and places of the weeks that will take place this year.
+</p>
+<ul>
+  <li>e-mail: rando@equisud.com</li>
+  <li>telefonisch: +(33) 7 79 47 65</li>
+</ul>
